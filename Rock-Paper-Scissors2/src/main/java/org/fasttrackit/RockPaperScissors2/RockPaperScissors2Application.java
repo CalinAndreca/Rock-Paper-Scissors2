9754421dfe30bbd3b76a9a0de0d9f8d27027227b
Calin Andreca <calin.andreca@gmail.com>
2023-08-03 19:08:45 +0300

    Initial commit

---
+++ Rock-Paper-Scissors2/src/main/java/org/fasttrackit/RockPaperScissors2/RockPaperScissors2Application.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.RockPaperScissors2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockPaperScissors2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockPaperScissors2Application.class, args);
+	}
+
+}
---
+++ Rock-Paper-Scissors2/src/test/java/org/fasttrackit/RockPaperScissors2/RockPaperScissors2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.RockPaperScissors2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockPaperScissors2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
